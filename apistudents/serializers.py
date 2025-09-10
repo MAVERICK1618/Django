6381ed4ee015ca5e178c977458b99255e8081746
MAVERICK1618 <arunindhumathi555@gmail.com>
2025-09-10 18:56:19 +0530

    Learning Class Based view

--- apistudents/serializers.py
+++ apistudents/serializers.py
@@ -1,8 +1,14 @@
 from rest_framework import serializers
 from students.models import *
+from employee.models import Employee
 
 
 class Studentserializer( serializers.ModelSerializer ):
     class Meta:
         model = Students
+        fields = "__all__"
+    
+class Employeeserializer(serializers.ModelSerializer ):
+    class Meta:
+        models = Employee
         fields = "__all__"
--- students/models.py
+++ students/models.py
@@ -4,6 +4,7 @@ class Students( models.Model ):
     student_id = models.CharField(max_length=10)
     name = models.CharField(max_length=50)
     Branch = models.CharField(max_length=50)
+    
 
     def __str__(self):
         return self.name
