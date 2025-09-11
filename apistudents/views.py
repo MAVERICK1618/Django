e13bf2b24ada6a31d0802f8d6e75e66565235847
MAVERICK1618 <arunindhumathi555@gmail.com>
2025-09-11 11:43:20 +0530

    Learn Class based Put in Employee

--- apistudents/views.py
+++ apistudents/views.py
@@ -45,14 +45,19 @@ def apiStudentPK(request , pk):
         student.delete()
         return Response(status=status.HTTP_204_NO_CONTENT) 
     
-
+ 
 
 #  creating Class Based Views
-
 
 class Employee( APIView ):
     def get( self , request ):
         employee = employeemodel.objects.all()
         serilaizer = Employeeserializer( employee , many = True )
-        return Response( serilaizer.data , status=status.HTTP_200_OK )        
+        return Response( serilaizer.data , status=status.HTTP_200_OK )  
+    def put(self , request):
+        serializer = Employeeserializer( data = request.data )
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data , status=status.HTTP_201_CREATED)
+        return Response(status=status.HTTP_400_BAD_REQUEST)      
    
